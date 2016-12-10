9932af3355ab152674186846bec8b0bf41ac8781
Yale Levine <Yale Levine>
2016-12-09 17:26:45 -0800

    adding preliminary score page with population  from DB

--- public/js/scores.js
+++ public/js/scores.js
@@ -4,9 +4,23 @@ $(function () { //Functions in this scope will run when the document ready even 
     $.ajax({
         type: "GET",
         url: "http://ec2-35-165-233-39.us-west-2.compute.amazonaws.com:3000/api", //Change this if using web address..
+      //  url:'http://localhost:3000/api',
         dataType: "json",
         success: function (data){
-            //BUILD elements in here using the data that was returned
+            var tbody= $('#scores');
+            data.forEach(function (datum,index) {
+                var tr = $('<tr>');
+                var rank = $('<td>');
+                var score = $('<td>');
+                var name = $('<td>');
+                rank.html(index+1+'.');
+                tr.append(rank);
+                score.html(datum.score);
+                tr.append(score);
+                name.html(datum.name);
+                tr.append(name);
+                tbody.append(tr);
+            });
         }
     });
 });
